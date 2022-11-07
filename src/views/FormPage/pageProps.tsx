6200e7f4e35d8e5a03535b2d1ddbb7d49625698b
kennthKun <c_kunx@163.com>
2022-11-07 16:25:11 +0800

    feat: .

--- src/views/FormPage/pageProps.tsx
+++ src/views/FormPage/pageProps.tsx
@@ -13,7 +13,7 @@ export default {
     },
     "layout": {
       "labelCol": {
-
+        span: 4
       },
       "wrapperCol": {
 
@@ -23,228 +23,189 @@ export default {
   },
   "FormList": [
     {
-      $id: 23,
-      valueType: "@group",
-      FormItemProps: {
-        grid: 1,
-      },
-      ControlProps: {
-        Content: [
+      "$id": "74f3f68b-9fc0-4f9f-83a1-79f6e9a4063b",
+      "valueType": "@group",
+      "ControlProps": {
+        "Content": [
           {
-            $id: 24,
-            valueType: "table",
+            "$id": "34a998f7-d11a-4cea-805a-28ebb9199ac9",
+            "valueType": "DoubleUploadImg",
+            "FormItemProps": {
+              "label": "双图上传",
+              "name": [
+                "34a998f7-d11a-4cea-805a-28ebb9199ac9",
+                "45b50a91-6f3c-4fd2-945a-5577bcf898c5"
+              ],
+              "required": false,
+              "style": {
+
+              }
+            }
+          },
+          {
+            "$id": "c6740486-198d-4652-a07f-0d5a2f99a89d",
+            "valueType": "DoubleUploadImg",
+            "FormItemProps": {
+              "label": "双图上传",
+              "name": [
+                "c6740486-198d-4652-a07f-0d5a2f99a89d",
+                "7543b282-e3e9-432b-8f92-e22b5272619b"
+              ],
+              "required": false,
+              "style": {
+
+              }
+            }
+          },
+          {
+            "valueType": "UploadImg",
+            "$id": "d7a03f56-0a19-4ee3-8d05-803d19d45fc0",
+            "FormItemProps": {
+              "label": "图片上传",
+              "name": "d7a03f56-0a19-4ee3-8d05-803d19d45fc0",
+              "required": false
+            }
+          },
+          {
+            "$id": "d5e826d0-4d48-4b8a-8989-f0f1aff6af54",
+            "valueType": "textarea",
+            "FormItemProps": {
+              "label": "文本域",
+              "name": "d5e826d0-4d48-4b8a-8989-f0f1aff6af54"
+            }
+          },
+          {
+            "$id": "e8be4814-07c5-41fe-9af9-11a06df8cb9a",
+            "valueType": "date",
+            "FormItemProps": {
+              "label": "日期",
+              "name": "e8be4814-07c5-41fe-9af9-11a06df8cb9a"
+            }
+          },
+          {
+            "$id": "d33d7865-e1e6-4c3d-a47f-c35e445b51ff",
+            "valueType": "dateTime",
+            "FormItemProps": {
+              "label": "日期时间",
+              "name": "d33d7865-e1e6-4c3d-a47f-c35e445b51ff"
+            }
+          },
+          {
+            "$id": "11183edf-082d-4ecc-b99c-da500602f648",
+            "valueType": "dateRange",
+            "FormItemProps": {
+              "label": "日期区间",
+              "name": "11183edf-082d-4ecc-b99c-da500602f648"
+            }
+          },
+          {
+            "$id": "52e64fd4-e4e6-4d18-bf3a-292d7ee09fe1",
+            "valueType": "dateTimeRange",
+            "FormItemProps": {
+              "label": "日期时间区间",
+              "name": "52e64fd4-e4e6-4d18-bf3a-292d7ee09fe1"
+            }
           }
         ]
       }
     },
     {
-      $id: 22,
-      valueType: "@group",
-      ControlProps: {
-        Content: [
+      "$id": "02b92dc2-3ab0-43a7-8213-8ff6d78d7e56",
+      "valueType": "@group",
+      "ControlProps": {
+        "Content": [
           {
-            $id: 17,
-            valueType: "IDCard",
-            ocr: 'idCardLicenseV2',
-            FormItemProps: {
-              label: '身份证',
-              name: [18, 19],
-            },
+            "$id": "13d3f11a-3fe0-4264-a85c-ca4fbb8da2ed",
+            "valueType": "DoubleUploadImg",
+            "FormItemProps": {
+              "label": "双图上传",
+              "name": [
+                "13d3f11a-3fe0-4264-a85c-ca4fbb8da2ed",
+                "37115627-ca42-4212-9ac1-002877328e8e"
+              ],
+              "required": false,
+              "style": {
+
+              }
+            }
           },
           {
-            $id: 14,
-            valueType: "mobile",
-            FormItemProps: {
-              label: '手机号',
-              name: 14,
-            },
+            "$id": "3d7770e6-a5af-4f8c-affe-1966d496a901",
+            "valueType": "text",
+            "FormItemProps": {
+              "label": "文本",
+              "name": "3d7770e6-a5af-4f8c-affe-1966d496a901"
+            }
           },
-        ],
-      },
-    },
-    {
-      $id: 21,
-      valueType: "checkbox",
-      FormItemProps: {
-        label: '多选框',
-        name: 21,
-      },
-      ControlProps: {
-        options: [
-          { label: "选项一", value: "1" },
-          { label: "选项二", value: "2" }
-        ],
-      },
-    },
-    {
-      valueType: 'UploadImg',
-      $id: 20,
-      FormItemProps: {
-        label: '图片上传',
-        name: 20,
-        required: false,
-      },
-    },
-    {
-      $id: 17,
-      valueType: "IDCard",
-      ocr: 'idCardLicenseV2',
-      FormItemProps: {
-        label: '身份证',
-        name: [18, 19],
-      },
-    },
-    {
-      $id: 16,
-      valueType: "DoubleUploadImg",
-      FormItemProps: {
-        label: '双图上传',
-        name: [16, 17],
-      },
-    },
-    {
-      $id: 15,
-      valueType: 'inputSearch',
-      FormItemProps: {
-        label: '搜索输入框',
-        name: 15,
-      },
-    },
-    {
-      $id: 14,
-      valueType: "mobile",
-      FormItemProps: {
-        label: '手机号',
-        name: 14,
-      },
-    },
-    {
-      $id: 13,
-      valueType: 'dateTimeRange',
-      FormItemProps: {
-        label: '日期时间区间',
-        name: 13,
-      },
-    },
-    {
-      $id: 12,
-      valueType: 'dateTime',
-      FormItemProps: {
-        label: '日期时间',
-        name: 12,
-      },
-    },
-    {
-      $id: 11,
-      valueType: 'dateRange',
-      FormItemProps: {
-        label: '日期区间',
-        name: 11,
-      },
-    },
-    {
-      $id: 10,
-      valueType: 'date',
-      FormItemProps: {
-        label: '日期',
-        name: 10,
+          {
+            "$id": "5f7db252-22b9-4d44-b831-53a9849e7ccd",
+            "valueType": "select",
+            "FormItemProps": {
+              "label": "下拉框",
+              "name": "5f7db252-22b9-4d44-b831-53a9849e7ccd"
+            },
+            "options": [
+
+            ]
+          },
+          {
+            "$id": "5578b972-cbe2-4a48-a364-5b3dfc6abc72",
+            "valueType": "switch",
+            "FormItemProps": {
+              "label": "开关",
+              "name": "5578b972-cbe2-4a48-a364-5b3dfc6abc72"
+            }
+          },
+          {
+            "$id": "9be05541-ccaf-4e6a-8754-9ab9e4737596",
+            "valueType": "password",
+            "FormItemProps": {
+              "label": "密码",
+              "name": "9be05541-ccaf-4e6a-8754-9ab9e4737596"
+            }
+          },
+          {
+            "$id": "cd623ef9-4e0a-4bd1-835c-2ab05bef8753",
+            "valueType": "digit",
+            "FormItemProps": {
+              "label": "数字输入框",
+              "name": "cd623ef9-4e0a-4bd1-835c-2ab05bef8753"
+            }
+          },
+          {
+            "$id": "d2e19511-2fde-493d-b4a2-840b3f9b6996",
+            "valueType": "money",
+            "FormItemProps": {
+              "label": "金额",
+              "name": "d2e19511-2fde-493d-b4a2-840b3f9b6996"
+            }
+          }
+        ]
       }
     },
     {
-      $id: 9,
-      valueType: 'textarea',
+      "$id": "be49b9cb-baeb-41ab-aec8-627f68ea9a51",
+      "valueType": "@group",
       FormItemProps: {
-        label: '文本域',
-        name: 9,
+        grid: 1,
       },
-    },
-    {
-      $id: 8,
-      valueType: 'money',
-      FormItemProps: {
-        label: '金额',
-        name: 8,
-      },
-    },
-    {
-      $id: 7,
-      valueType: 'digit',
-      FormItemProps: {
-        label: '数字输入框',
-        name: 7,
-      },
-    },
-    {
-      $id: 6,
-      valueType: 'password',
-      FormItemProps: {
-        label: '密码',
-        name: 6,
+      "ControlProps": {
+        "Content": [
+          {
+            "$id": "d3f64b8f-6e09-4aba-984e-7c5a58ea6441",
+            "valueType": "table"
+          }
+        ]
       }
     },
     {
-      $id: 5,
-      valueType: 'switch',
-      FormItemProps: {
-        label: '开关',
-        name: 5,
-      }
-    },
-    {
-      $id: 1,
-      valueType: "radio",
-      FormItemProps: {
-        label: '单选框',
-        name: 1,
-      },
-      ControlProps: {
-        options: [
-          { label: "选项一", value: "1" },
-          { label: "选项二", value: "2" }
-        ],
-      },
-    },
-    {
-      $id: 2,
-      valueType: "text",
-      FormItemProps: {
-        label: '文本',
-        name: '2',
-      },
-    },
-    {
-      $id: 3,
-      valueType: 'select',
-      FormItemProps: {
-        label: '下拉框',
-        name: 3,
-      },
-      options: [{
-        label: "1",
-        value: "22"
-      }]
-    },
-    {
-      $id: 4,
-      valueType: 'select',
-      FormItemProps: {
-        label: '下拉框',
-        name: 4,
-      },
-      ControlFieldNames: {
-        valueName: "fieldTypeId",
-        labelName: "typeDesc",
-      },
-      request: {
-        url: "https://dev.ailieyun.com/api/custom/businessGoodsFieldType/list",
-        type: "post",
-        params: { data: {} },
-        getData: ["data", "data"]
-      },
-    },
-    {
-      $id: 0,
-      valueType: "BottomSubButton",
+      "$id": "af78442b-a63e-49d1-9a70-699d9c10b063",
+      "valueType": "BottomSubButton"
     }
   ],
+  "modalList": [
+    {
+
+    }
+  ]
 }
